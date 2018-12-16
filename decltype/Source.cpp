9eb6cd79978c2284ccad193bc43a56ceee6e29a4
Yusuke Mori <fr052123@gmail.com>
2018-12-16 23:51:44 +0900

    Update Source.cpp

--- decltype/Source.cpp
+++ decltype/Source.cpp
@@ -7,19 +7,39 @@ template <typename T>
 struct has_size_type {
 private:
     template <typename U>
-    static auto test(U u) -> decltype(typename U::size_type, std::true_type);
-    static auto test(...) -> decltype(std::false_type);
+    static auto test(int) -> decltype(std::declval<typename U::size_type>(), std::true_type());
+
+    template <typename U>
+    static auto test(...) -> decltype(std::false_type());
 
 public:
-    using type = decltype(test(std::declval<T>()));
+    using type = decltype(test<T>(0));
     static constexpr bool value = type::value;
 };
+
+template <typename T>
+struct has_size {
+private:
+    template <typename U>
+    static auto test(int) -> decltype(std::declval<U>().size(), std::true_type());
+
+    template <typename U>
+    static auto test(...) -> decltype(std::false_type());
+
+public:
+    using type = decltype(test<T>(0));
+    static constexpr bool value = type::value;
+};
+
 
 int main() {
     std::cout
         << std::boolalpha
-        << has_size_type<std::vector<int> >::value << std::endl  // true
-        << has_size_type<int>::value << std::endl;               // false
+        << has_size_type<std::vector<int>>::value << std::endl   // true
+        << has_size_type<int>::value << std::endl                // false
+        << has_size<std::vector<int>>::value << std::endl        // true
+        << has_size<int>::value << std::endl;                    // false
+
 
     return 0;
 }
